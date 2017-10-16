1dabac104ddcf01b721dfbbe0d7bb7e884277c77
Premsuraj <premsrj@gmail.com>
2017-10-16 15:54:26 +0530

    Added a fragment to the home activity

---
+++ app/src/main/java/com/premsuraj/expensemanager/home/HomeActivityFragment.kt
@@ -0,0 +1,19 @@
+package com.premsuraj.expensemanager.home
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.premsuraj.expensemanager.R
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+class HomeActivityFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+}
