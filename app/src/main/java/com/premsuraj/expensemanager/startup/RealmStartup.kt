73db068eec5b4c2b2a20518f9e7885c707f30369
Premsuraj <premsrj@gmail.com>
2017-10-24 11:27:30 +0530

    Add default categories during app startup

--- app/src/main/java/com/premsuraj/expensemanager/startup/RealmStartup.kt
+++ app/src/main/java/com/premsuraj/expensemanager/startup/RealmStartup.kt
@@ -2,6 +2,7 @@ package com.premsuraj.expensemanager.startup
 
 import com.premsuraj.expensemanager.Constants.REALM.AUTH_URL
 import com.premsuraj.expensemanager.Constants.REALM.REALM_URL
+import com.premsuraj.expensemanager.data.Category
 import io.realm.*
 
 class RealmStartup : StartupItem() {
@@ -16,6 +17,7 @@ class RealmStartup : StartupItem() {
 
                 val defaultConfig = SyncConfiguration.Builder(result!!, REALM_URL).build()
                 Realm.setDefaultConfiguration(defaultConfig)
+                setupDefaultData()
                 callback?.onItemLoaded(this@RealmStartup)
             }
 
@@ -23,6 +25,53 @@ class RealmStartup : StartupItem() {
                 callback?.onItemLoadFailed(this@RealmStartup)
             }
         })
+    }
+
+    private fun setupDefaultData() {
+        val instance = Realm.getDefaultInstance()
+        if (instance.where(Category::class.java).count() == 0L) {
+            instance.beginTransaction()
+            instance.insertOrUpdate(getDefaultCategoryList())
+            instance.commitTransaction()
+        }
+    }
+
+    private fun getDefaultCategoryList(): List<Category> {
+        val list = ArrayList<Category>()
+
+        list.add(Category()) //other
+        val food = Category("", "Food", "")
+        food.id = "" + food.hashCode()
+        list.add(food)
+        val snacks = Category("", "Snacks", food.id)
+        snacks.id = "" + snacks.hashCode()
+        list.add(snacks)
+        val fruits = Category("", "Fruits", food.id)
+        fruits.id = "" + fruits.hashCode()
+        list.add(fruits)
+
+        val family = Category("", "Family", "")
+        family.id = "" + family.hashCode()
+        list.add(family)
+        val outing = Category("", "Outing", family.id)
+        outing.id = "" + outing.hashCode()
+        list.add(outing)
+
+        val sana = Category("", "Sana", "")
+        sana.id = "" + sana.hashCode()
+        list.add(sana)
+        val sanaFood = Category("", "Food", sana.id)
+        sanaFood.id = "" + sanaFood.hashCode()
+        list.add(sanaFood)
+
+        val retirement = Category("", "Retirement", "")
+        retirement.id = "" + retirement.hashCode()
+        list.add(retirement)
+        val mutualFund = Category("", "Mutual Funds", retirement.id)
+        mutualFund.id = "" + mutualFund.hashCode()
+        list.add(mutualFund)
+
+        return list
     }
 
 }
